e13cb2ac91566cf0110c5bcc4cf8267815d587c2
Rio Agustian Gilang Fernando <92105966+rioAgustian@users.noreply.github.com>
2022-05-22 17:37:35 +0700

    Add files via upload

---
+++ PRAKTIKUM_3/pelemahan_sinyal.m
@@ -0,0 +1,21 @@
+clc
+% sinyal awal
+T = 100;
+t = 0:1/T:2;
+f1 = 1;
+y1 = sin(2*pi*t);
+
+% sinyal setelah pelemahan
+a = 0.1; % nilai a harus < 1
+y1_att = a*sin(2*pi*t);
+
+% 1st plot
+subplot(2,1,1);
+plot(t, y1);
+title('Sinyal awal');
+
+% 2nd plot
+subplot(2,1,2);
+plot(t, y1_att);
+ylim([min(y1), max(y1)]);
+title('Sinyal setelah atenuasi');
